dad0fc89d83ef6de1da5cbd01eed056edd51c17d
Himanshu Madhura <himanshumadhura02@gmail.com>
2023-04-21 15:59:11 +0530

    Location Permission added

--- app/src/main/java/com/example/liveforcast/HomeScreen.java
+++ app/src/main/java/com/example/liveforcast/HomeScreen.java
@@ -23,6 +23,7 @@ import com.google.android.gms.tasks.Task;
 public class HomeScreen extends AppCompatActivity {
 
     FusedLocationProviderClient flpc;
+    int PERMISSION_ID = 44;
     double longitude, latitude;
 
     @Override
@@ -35,42 +36,53 @@ public class HomeScreen extends AppCompatActivity {
     }
 
     public void getLastLocation() {
-        if (isLocationEnabled()) {
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            flpc.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    Location loc = task.getResult();
-                    if (loc == null) {
-                        Log.d("Res", String.valueOf(loc));
-                    } else {
-                        longitude = loc.getLongitude();
-                        latitude = loc.getLatitude();
-                        Log.d("Longitude", String.valueOf(longitude));
-                        Log.d("Latitude", String.valueOf(latitude));
-                    }
+        if (CheckPermission()) {
+            if (isLocationEnabled()) {
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
                 }
-            });
-        }
-        else{
-            Toast.makeText(HomeScreen.this, "Loaction is disabled. Please turn it on...", Toast.LENGTH_LONG).show();
-            Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(i);
+                flpc.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location loc = task.getResult();
+                        if (loc == null) {
+                            Log.d("Res", String.valueOf(loc));
+                        } else {
+                            longitude = loc.getLongitude();
+                            latitude = loc.getLatitude();
+                            Log.d("Longitude", String.valueOf(longitude));
+                            Log.d("Latitude", String.valueOf(latitude));
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(HomeScreen.this, "Loaction is disabled. Please turn it on...", Toast.LENGTH_LONG).show();
+                            Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(i);
+            }
+        }else{
+            requestPermissions();
         }
     }
 
     public boolean isLocationEnabled(){
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         return lm.isProviderEnabled(LocationManager.GPS_PROVIDER) || lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    public boolean CheckPermission(){
+        return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void requestPermissions(){
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
     }
 }
 
