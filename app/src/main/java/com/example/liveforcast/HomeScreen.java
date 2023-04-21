6d96ef734a847a0f43d05fec9179e94ce3de1851
Himanshu Madhura <himanshumadhura02@gmail.com>
2023-04-21 10:12:09 +0530

    added splashscreen

---
+++ app/src/main/java/com/example/liveforcast/HomeScreen.java
@@ -0,0 +1,13 @@
+package com.example.liveforcast;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+    }
+}
---
+++ app/src/main/java/com/example/liveforcast/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.liveforcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getSupportActionBar().hide();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        GifImageView gifImageView = (GifImageView) findViewById(R.id.splashgif);
+        gifImageView.setGifImageResource(R.drawable.bg1);
+        TextView txt = findViewById(R.id.splashtxt);
+
+        Animation animImg = AnimationUtils.loadAnimation(this, R.anim.splashanimimg);
+        gifImageView.startAnimation(animImg);
+        Animation animtxt = AnimationUtils.loadAnimation(this, R.anim.splashanimtxt);
+        txt.startAnimation(animtxt);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, HomeScreen.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
